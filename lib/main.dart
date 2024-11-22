2017bf66b411374e3704cc4ac7dcd8a220bb8c4d
Fhsley <abiskaroli21@gmail.com>
2024-11-22 00:07:03 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,40 +7,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Simple Button App',
-      theme: ThemeData(
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(),
-    );
-  }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  void _onButtonPressed() {
-    print('You clicked button');
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromRGBO(133, 207, 245, 1), // Background color
-      body: Center(
-        child: ElevatedButton(
-          onPressed: _onButtonPressed,
-          style: ElevatedButton.styleFrom(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20),
-            ),
-          ),
-          child: const Text('Button'),
-        ), 
-      ),
-    );
-  }
+class StatelessWidget {
 }
